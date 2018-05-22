b3dd99aa2cfd7c215313408f3b0e27ebd9a8c052
Sam <sam@thriveweb.com.au>
2018-05-22 15:23:26 +1000

    pingback

--- header.php
+++ header.php
@@ -20,7 +20,11 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
 
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<?php
+	if ( is_singular() && pings_open() ) {
+		printf( '<link rel="pingback" href="%s">' . "\n", esc_url( get_bloginfo( 'pingback_url' ) ) );
+	}
+	?>
 
 	<?php wp_head(); ?>
 </head>
